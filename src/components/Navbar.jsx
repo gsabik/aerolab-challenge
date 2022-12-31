a32f309a9fb57a7a17a7217848ebcd8d082b6cc5
Guido Sabik <guido.sabik@gmail.com>
2022-12-30 22:44:13 -0300

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,42 @@
+import { 
+	Box, 
+	Button, 
+	Container, 
+	HStack,
+	Image,
+	Text
+} from "@chakra-ui/react";
+import logo from "../assets/icons/aerolab-logo-1.svg";
+import chevronActive from "../assets/icons/chevron-active.svg";
+import aeropay from "../assets/icons/aeropay-1.svg";
+
+const Navbar = () => {
+	return (
+		<Box>
+			<Container maxW="6xl">
+				<HStack
+					alignItems="center"
+					as="nav"
+					justifyContent="space-between" 
+					py={6}
+				>
+					<Image src={logo}/>
+					<Button
+						bgColor="white"
+						boxShadow="sm"
+						border="1px"
+						borderColor="gray.200"
+						borderRadius="xl"
+					>
+						<HStack>
+							<Image src={aeropay}/>
+							<Text>10.000</Text>
+						</HStack>
+					</Button>
+				</HStack>
+			</Container>
+		</Box>
+	);
+}
+
+export default Navbar
---
+++ src/layout/Layout.jsx
@@ -0,0 +1,12 @@
+import { Flex } from "@chakra-ui/react";
+import Navbar from "../components/Navbar";
+
+const Layout = () => {
+	return (
+		<Flex direction="column">
+			<Navbar/>
+		</Flex>
+	);
+}
+
+export default Layout
