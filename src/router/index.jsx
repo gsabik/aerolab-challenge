3226c6b8c7d410981aceaa241d6ef6d8bee71278
Guido Sabik <guido.sabik@gmail.com>
2023-01-02 15:25:13 -0300

    create RedeemHistory page and add to router

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import Layout from "../layout/Layout";
 import Home from "../pages/Home";
+import RedeemHistory from "../pages/RedeemHistory";
 
 export const router = createBrowserRouter([
 	{
@@ -10,6 +11,10 @@ export const router = createBrowserRouter([
 			{
 				index: true,
 				element: <Home/>
+			},
+			{
+				path: "/history",
+				element: <RedeemHistory/>
 			}
 		]
 	}
