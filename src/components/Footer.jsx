51a3c6f2438adcde446db4214fd2a17ee135566c
Guido Sabik <guido.sabik@gmail.com>
2023-01-19 22:27:11 -0300

    create Footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,27 @@
+import { 
+	Flex,
+	HStack, 
+	Image, 
+	Text
+} from "@chakra-ui/react";
+import githubDefault from "../assets/icons/github-default.svg";
+
+const Footer = () => {
+	return (
+		<Flex 
+			alignItems="center" 
+			justifyContent="center" 
+			h="200px" 
+			w="full"
+		>
+			<a href="https://github.com/gsabik/aerolab-challenge" target="_blank">
+				<HStack>
+					<Image alt={githubDefault} src={githubDefault}/>
+					<Text color="neutral.600" fontWeight="semibold">View this repository</Text>
+				</HStack>
+			</a>
+		</Flex>
+	);
+}
+
+export default Footer
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import { Flex } from "@chakra-ui/react";
 import Hero from "../components/Hero";
 import ProductList from "../components/ProductList";
+import Footer from "../components/Footer";
 
 const Home = () => {
 	return (
 		<Flex direction="column">
 			<Hero/>
 			<ProductList/>
+			<Footer/>
 		</Flex>
 	);
 }
