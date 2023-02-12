a5d23841cc29c9e682d8ac63f3ccae14f94ad268
Guido Sabik <guido.sabik@gmail.com>
2023-02-12 12:14:05 -0300

    creat headers object on api file

--- src/api/api.js
+++ src/api/api.js
@@ -1,15 +1,16 @@
 const BASE_URL = "https://coding-challenge-api.aerolab.co";
 const TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2M2FlMGEzNmQ0ODcxNjAwMWFkMjY5ZDIiLCJpYXQiOjE2NzIzNTAyNjJ9.jaE3dJtdoPQeorxCWKwC8N2TRWDmzp-PricVzuzh6kk";
+const HEADERS = {
+	"Content-Type": "application/json",
+	"Accept": "application/json",
+	"Authorization": `Bearer ${TOKEN}`
+};
 
 export const requestUser = async() => {
 	const response = await fetch(`${BASE_URL}/user/me`, {
 		method: "GET",
 		withCredentials: true,
-		headers: {
-			"Content-Type": "application/json",
-			"Accept": "application/json",
-			"Authorization": `Bearer ${TOKEN}`
-		}
+		headers: HEADERS
 	});
 
 	const user = await response.json();
@@ -21,11 +22,7 @@ export const requestProducts = async() => {
 	const response = await fetch(`${BASE_URL}/products`, {
 		method: "GET",
 		withCredentials: true, 
-		headers: {
-			"Content-Type": "application/json",
-			"Accept": "application/json",
-			"Authorization": `Bearer ${TOKEN}`
-		}
+		headers: HEADERS
 	});
 
 	const products = await response.json();
@@ -37,11 +34,7 @@ export const requestRedeemHistory = async() => {
 	const response = await fetch(`${BASE_URL}/user/history`, {
 		method: "GET",
 		withCredentials: true, 
-		headers: {
-			"Content-Type": "application/json",
-			"Accept": "application/json",
-			"Authorization": `Bearer ${TOKEN}`
-		}
+		headers: HEADERS
 	});
 
 	const redeemHistory = await response.json();
@@ -54,11 +47,7 @@ export const postPoints = async(points) => {
 		method: "POST",
 		withCredentials: true,
 		body: JSON.stringify({ "amount": points }),
-		headers: {
-			"Content-Type": "application/json",
-			"Accept": "application/json",
-			"Authorization": `Bearer ${TOKEN}`
-		}
+		headers: HEADERS
 	});
 
 	const postPoints = await response.json();
@@ -70,11 +59,7 @@ export const postProduct = async(id) => {
 	const response = await fetch(`${BASE_URL}/redeem`, {
 		method: "POST",
 		body: JSON.stringify({ "productId": id }),
-		headers: {
-			"Content-Type": "application/json",
-			"Accept": "application/json",
-			"Authorization": `Bearer ${TOKEN}`
-		}
+		headers: HEADERS
 	});
 
 	const redeem = await response.json();
